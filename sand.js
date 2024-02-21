dbe4129502933685a4d4261883f0daecd9cfde4b
kellentow <125595067+kellentow@users.noreply.github.com>
2024-02-20 18:06:52 -0600

    added various features
    1. temperature
    2. particle info
    3. corrected mouse position

--- sand.js
+++ sand.js
@@ -5,51 +5,51 @@ const targetFrameDuration = 1000 / targetFPS;
 let lastFrameTime = performance.now();
 
 var sands = []
-var fps = {now:0, round:0, array:[]}
+var fps = { now: 0, round: 0, array: [] }
 var last = new Date();
 var sand = []
 var brushS = 1
 var occupied = []
-var scr = {d:300,dm1:0}
-scr.dm1 = scr.d-1
-var mouse = {x:0,y:0,h:{id:null,class:null}}
+var scr = { d: 300, dm1: 0 }
+scr.dm1 = scr.d - 1
+var mouse = { x: 0, y: 0, h: { id: null, class: null } }
 var mods = {
-    update:function(){},
-    list:[]
+    update: function () { },
+    list: []
 }
 var powders = {
-    Sand:{
-        convection: 0.002,
+    Sand: {
+        convection: 0.01,
         color: "#D2B48C",
         temp: 30,
         kind: "powder"
     },
     MoltenGlass: {
-        convection: 0.0005,
+        convection: 0.03,
         color: "#996633",
         temp: 1700,
         kind: "liquid"
     },
     Glass: {
-        convection: 0.0007,
+        convection: 0.005,
         color: "#24424a",
         temp: 30,
         kind: "solid"
     },
     Ice: {
-        convection: 0.0005,
+        convection: 0.01,
         color: "#2e93db",
         temp: 0,
         kind: "solid"
     },
     Water: {
-        convection: 0.0007,
+        convection: 0.01,
         color: "#0034cf",
         temp: 30,
         kind: "liquid"
     },
     WaterVapor: {
-        convection: 0.0003,
+        convection: 0.01,
         color: "#4b4bf2",
         temp: 100,
         kind: "gas"
@@ -61,7 +61,7 @@ var sel = 0
 function updateloop() {
     const currentTime = performance.now();
     const elapsed = currentTime - lastFrameTime;
-    
+
     occupied = []
     isParticleAt()
     mousehandler();
@@ -71,12 +71,12 @@ function updateloop() {
 
     // fps calc
     var thisLoop = new Date();
-    fps.now = Math.round((1000 / (thisLoop - last))*10)/10
+    fps.now = Math.round((1000 / (thisLoop - last)) * 10) / 10
     fps.array.unshift(fps.now);
     for (let i = 0; i < fps.array.length; i++) {
         fps.round += fps.array[i];
     }
-    fps.round = Math.round((fps.round / fps.array.length)*10)/10
+    fps.round = Math.round((fps.round / fps.array.length) * 10) / 10
     if (fps.array.length > 20) {
         fps.array.pop()
     }
@@ -85,7 +85,7 @@ function updateloop() {
     // update for 60fps
     lastFrameTime = currentTime;
     const timeToNextFrame = Math.max(0, targetFrameDuration - elapsed);
-    setTimeout(() => {requestAnimationFrame(updateloop);}, timeToNextFrame);
+    setTimeout(() => { requestAnimationFrame(updateloop); }, timeToNextFrame);
 }
 
 function isParticleAt(x, y) {
@@ -114,7 +114,7 @@ function getParticleAt(x, y) {
             if (!sands[particle.x]) {
                 sands[particle.x] = [];
             }
-            sands[particle.x][particle.y] = {particle: particle, index: i};
+            sands[particle.x][particle.y] = { particle: particle, index: i };
         }
     }
     if (sands[x]) {
@@ -122,13 +122,15 @@ function getParticleAt(x, y) {
             return sands[x][y];
         }
     }
-    return {particle: "null", index: -1};
+    return { particle: "null", index: -1 };
 }
 
 function update() {
     for (var i = 0; i < sand.length; i++) {
         var px = sand[i].x + 0;
         var py = sand[i].y + 0;
+
+        //update particle position
         if (sand[i].type == "Sand") {
             if (!isParticleAt(sand[i].x, sand[i].y + 1)) {
                 sand[i].y += 1;
@@ -138,8 +140,11 @@ function update() {
             } else if (!isParticleAt(sand[i].x + 1, sand[i].y + 1)) {
                 sand[i].x += 1;
                 sand[i].y += 1;
-            } 
-            if (sand[i].temp >= 1700) {sand[i].type = "MoltenGlass"}
+            }
+            if (sand[i].temp >= 1700) {
+                sand[i].type = "MoltenGlass"
+                return
+            }
 
         } else if (sand[i].type == "MoltenGlass") {
             if (!isParticleAt(sand[i].x, sand[i].y + 1)) {
@@ -154,15 +159,21 @@ function update() {
                 sand[i].x -= 1;
             } else if (!isParticleAt(sand[i].x + 1, sand[i].y)) {
                 sand[i].x += 1;
-            }  
-            if (sand[i].temp < 1700) {sand[i].type = "Glass"}
-            var part1 = getParticleAt(sand[i].x, sand[i].y-1)
+            }
+            if (sand[i].temp < 1700) {
+                sand[i].type = "Glass"
+                return
+            }
+            var part1 = getParticleAt(sand[i].x, sand[i].y - 1)
             if (part1.particle.kind == "powder") {
                 sand[part1.index].y = sand[i].y;
                 sand[i].y -= 1;
             }
         } else if (sand[i].type == "Glass") {
-
+            if (sand[i].temp >= 1700) {
+                sand[i].type = "MoltenGlass"
+                return
+            }
         } else if (sand[i].type == "Water") {
             if (!isParticleAt(sand[i].x, sand[i].y + 1)) {
                 sand[i].y += 1;
@@ -176,10 +187,13 @@ function update() {
                 sand[i].x -= 1;
             } else if (!isParticleAt(sand[i].x + 1, sand[i].y)) {
                 sand[i].x += 1;
-            }  
-            if (sand[i].temp >= 100) {sand[i].type = "WaterVapor"}
-            if (sand[i].temp <= 0) {sand[i].type = "Ice"}
-            var part1 = getParticleAt(sand[i].x, sand[i].y-1)
+            }
+            if (sand[i].temp >= 100) {
+                sand[i].type = "WaterVapor"
+                return
+            }
+            if (sand[i].temp <= 0) { sand[i].type = "Ice" }
+            var part1 = getParticleAt(sand[i].x, sand[i].y - 1)
             if (part1.particle.kind == "powder") {
                 sand[part1.index].y = sand[i].y;
                 sand[i].y -= 1;
@@ -197,21 +211,81 @@ function update() {
                 sand[i].x -= 1;
             } else if (!isParticleAt(sand[i].x + 1, sand[i].y)) {
                 sand[i].x += 1;
-            }  
-            if (sand[i].temp < 100) {sand[i].type = "Water"}
-            var part1 = getParticleAt(sand[i].x, sand[i].y-1)
+            }
+            if (sand[i].temp < 100) {
+                sand[i].type = "Water"
+                return
+            }
+            var part1 = getParticleAt(sand[i].x, sand[i].y - 1)
             if (part1.particle.kind == "powder" || part1.particle.kind == "liquid") {
                 sand[part1.index].y = sand[i].y;
                 sand[i].y -= 1;
             }
         } else if (sand[i].type == "Ice") {
-            if (sand[i].temp > 0) {sand[i].type = "Water"}
+            if (sand[i].temp > 0) {
+                sand[i].type = "Water"
+                return
+            }
         }
-        if (sand[i].x < 1 ||sand[i].x >= scr.dm1 ||sand[i].y < 1 ||sand[i].y >= scr.dm1) {
+        if (sand[i].x < 1 || sand[i].x >= scr.dm1 || sand[i].y < 1 || sand[i].y >= scr.dm1) {
             // Reset particle position if out of bounds
             sand[i].x = px;
             sand[i].y = py;
         }
+
+        p1 = getParticleAt(sand[i].x, sand[i].y - 1)
+        p2 = getParticleAt(sand[i].x + 1, sand[i].y)
+        p3 = getParticleAt(sand[i].x, sand[i].y + 1)
+        p4 = getParticleAt(sand[i].x - 1, sand[i].y)
+
+        if (p1.index != -1) {
+            if (p1.particle.temp > sand[i].temp) {
+                var tempchange1 = p1.particle.temp * ((p1.particle.convection + sand[i].convection) / 2)
+                sand[i].temp += tempchange1
+                sand[p1.index].temp -= tempchange1
+            } else if (p1.particle.temp < sand[i].temp) {
+                var tempchange1 = sand[i].temp * ((p1.particle.convection + sand[i].convection) / 2)
+                sand[p1.index].temp += tempchange1
+                sand[i].temp -= tempchange1
+            }
+        }
+
+        if (p2.index != -1) {
+            if (p2.particle.temp > sand[i].temp) {
+                var tempchange2 = p2.particle.temp * ((p2.particle.convection + sand[i].convection) / 2)
+                sand[i].temp += tempchange2
+                sand[p2.index].temp -= tempchange2
+            } else if (p1.particle.temp < sand[i].temp) {
+                var tempchange2 = sand[i].temp * ((p2.particle.convection + sand[i].convection) / 2)
+                sand[p2.index].temp += tempchange2
+                sand[i].temp -= tempchange2
+            }
+        }
+
+        if (p3.index != -1) {
+            if (p3.particle.temp > sand[i].temp) {
+                var tempchange3 = p3.particle.temp * ((p3.particle.convection + sand[i].convection) / 2)
+                sand[i].temp += tempchange3
+                sand[p3.index].temp -= tempchange3
+            } else if (p3.particle.temp < sand[i].temp) {
+                var tempchange3 = sand[i].temp * ((p3.particle.convection + sand[i].convection) / 2)
+                sand[p3.index].temp += tempchange3
+                sand[i].temp -= tempchange3
+            }
+        }
+
+        if (p4.index != -1) {
+            if (p4.particle.temp > sand[i].temp) {
+                var tempchange4 = p4.particle.temp * ((p4.particle.convection + sand[i].convection) / 2)
+                sand[i].temp += tempchange4
+                sand[p4.index].temp -= tempchange4
+            } else if (p4.particle.temp < sand[i].temp) {
+                var tempchange4 = sand[i].temp * ((p4.particle.convection + sand[i].convection) / 2)
+                sand[p4.index].temp += tempchange4
+                sand[i].temp -= tempchange4
+            }
+        }
+
         occupied[px][py] = false;
         occupied[sand[i].x][sand[i].y] = true;
     }
@@ -238,33 +312,52 @@ function draw() {
     let ctx = canvas.getContext('2d');
 
     ctx.font = '13px Trebuchet MS ';
-    ctx.fillText('FPS: '+fps.round, 10, 15);
-    ctx.fillText(sand.length+' particles', 10, 30);
+    ctx.fillText('FPS: ' + fps.round, 10, 15);
+    ctx.fillText(sand.length + ' particles', 10, 30);
     ctx.fillText(selected[sel], 10, 60);
-    ctx.fillText('Brush size: '+brushS, 10, 76);
+    ctx.fillText('Brush size: ' + brushS, 10, 75);
+    var p = getParticleAt(mouse.x, mouse.y)
+    if (p.index !== -1) {
+        ctx.fillText('Particle info: ', 10, 105);
+        ctx.fillText('Type: ' + p.particle.type, 10, 120);
+        ctx.fillText('Temp: ' + p.particle.temp, 10, 135);
+        ctx.fillText('Position: ' + p.particle.x + "," + p.particle.y, 10, 150);
+        ctx.fillText('#' + p.index, 10, 165);
+    }
 }
 
 function init() {
     screen.create.screen(scr.d, scr.d, "sand");
-    screen.create.screen(3/8*scr.d, scr.d, "sidebar");
+    screen.create.screen(3 / 8 * scr.d, scr.d, "sidebar");
     for (let i = 0; i < 80; i++) {
         for (let j = 0; j < 80; j++) {
-            if (j>=40 && i>5 && i<75) {
-                sand.push(new Particle(90-i, 90-j, "Sand"))
-            } else if (j<40 && j>5 && i>5 && i<75) {
-                sand.push(new Particle(90-i, 90-j, "MoltenGlass"))
+            if (j >= 40 && i > 5 && i < 75) {
+                sand.push(new Particle(90 - i, 90 - j, "Sand"))
+            } else if (j < 40 && j > 5 && i > 5 && i < 75) {
+                sand.push(new Particle(90 - i, 90 - j, "MoltenGlass"))
             } else {
-                sand.push(new Particle(90-i, 90-j, "Glass"))
+                sand.push(new Particle(90 - i, 90 - j, "Glass"))
             }
         }
     }
     var loadModsEvent = new Event('LoadMods');
     setTimeout(document.dispatchEvent(loadModsEvent), 1000)
+
+    var a = document.getElementById("sand")
+    a.style.position = "absolute"
+    a.style.top = "150px"
+    a.style.left = "10px"
+
+    var b = document.getElementById("sidebar")
+    b.style.position = "absolute"
+    b.style.top = "150px"
+    b.style.left = "310px"
+
     updateloop();
 }
 
 class Particle {
-    constructor(x, y, type){
+    constructor(x, y, type) {
         this.temp = powders[type].temp
         this.color = powders[type].color
         this.convection = powders[type].convection
@@ -272,22 +365,22 @@ class Particle {
         this.type = type
         this.x = x
         this.y = y
-        
+
     }
 }
 
 var keys = {};
-document.addEventListener('keydown', function (e) {keys[e.code] = 1});
-document.addEventListener('keyup', function (e) {keys[e.code] = 3});
+document.addEventListener('keydown', function (e) { keys[e.code] = 1 });
+document.addEventListener('keyup', function (e) { keys[e.code] = 3 });
 document.addEventListener('mousemove', function (e) {
-    mouse.x = e.layerX-10
-    mouse.y = e.layerY-80
+    mouse.x = e.clientX - 10
+    mouse.y = e.clientY - 150
     mouse.h = e.target
 });
 
 //mouse stuff
-document.addEventListener('mousedown', function (e) {mouse.b = e.buttons});
-document.addEventListener('mouseup', function (e) {mouse.b = e.buttons});
+document.addEventListener('mousedown', function (e) { mouse.b = e.buttons });
+document.addEventListener('mouseup', function (e) { mouse.b = e.buttons });
 document.addEventListener('wheel', function (e) {
     if (e.deltaY < 0) {
         brushS += 1
@@ -311,16 +404,17 @@ function keyhandler() {
 }
 
 function mousehandler() {
+    console.log
     if (mouse.h.id == "sand" && mouse.h.className == "screen") {
         if (mouse.b == 1) {
-            var a = getPointsInRadius(brushS) 
+            var a = getPointsInRadius(brushS)
             for (let i = 0; i < a.length; i++) {
                 if (!occupied[a[i].x][a[i].y])
-                sand.push(new Particle(a[i].x, a[i].y, selected[sel]))
+                    sand.push(new Particle(a[i].x, a[i].y, selected[sel]))
             }
         }
         if (mouse.b == 2) {
-            var a = getPointsInRadius(brushS) 
+            var a = getPointsInRadius(brushS)
             for (let i = 0; i < a.length; i++) {
                 deleteParticle(a[i].x, a[i].y)
             }
@@ -334,7 +428,7 @@ function mousehandler() {
 
 function deleteParticle(x1, y1) {
     var a = getParticleAt(x1, y1).index
-    if (a!= -1) {
+    if (a != -1) {
         sand.splice(a, 1);
     }
 }
@@ -345,31 +439,30 @@ init()
 function calculateDistance(x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 }
-  
+
 // Function to get all points within a given radius around the mouse position
 function getPointsInRadius(radius) {
     let points = [];
-  
+
     // Determine the bounding box around the mouse position
     const minX = Math.max(0, Math.floor(mouse.x - radius));
     const maxX = Math.min(scr.d - 1, Math.ceil(mouse.x + radius));
     const minY = Math.max(0, Math.floor(mouse.y - radius));
     const maxY = Math.min(scr.d - 1, Math.ceil(mouse.y + radius));
-  
+
     // Iterate through points within the bounding box
     for (let x = minX; x <= maxX; x++) {
-      for (let y = minY; y <= maxY; y++) {
-        // Calculate the distance from the current point to the mouse position
-        const distance = calculateDistance(mouse.x, mouse.y, x, y);
-  
-        // Check if the distance is within the specified radius
-        if (distance <= radius) {
-          // Add the point to the array
-          points.push({ x, y });
+        for (let y = minY; y <= maxY; y++) {
+            // Calculate the distance from the current point to the mouse position
+            const distance = calculateDistance(mouse.x, mouse.y, x, y);
+
+            // Check if the distance is within the specified radius
+            if (distance <= radius) {
+                // Add the point to the array
+                points.push({ x, y });
+            }
         }
-      }
     }
-  
+
     return points;
-  }
-  
+}
